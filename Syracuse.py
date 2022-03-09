6a1a28ce3da6045797caf8a63d862e24134feb10
yakoubsmail <smail@et.esiea.fr>
2022-03-09 14:05:31 +0100

    ok

--- Syracuse.py
+++ Syracuse.py
@@ -1,23 +1,13 @@
-class Syracuse:
-    def __init__(self):
-        pass
-        with open("./number.txt", "r") as file:
-            number = file.read()
-        self.u = int(number)
-
-    def syracuse_(self):
-        f = open('Syracuse_de'+str(self.u)+'.txt','w')
-        iter =0
-        while(self.u!=1):
-            iter +=1
-            if self.u%2==0: 
-                self.u = self.u//2
-            else:
-                self.u = 3*self.u+1
-            print(self.u)
-            f.write(str(self.u)+', ')
-        print(iter," : iterations")
-        return self.u
-
-x = Syracuse()
-x.syracuse_()
+#
+def Collatz(a):
+    nbrlter=0
+    while a!=1:
+        if a%2==0:
+            a=a//2
+        else:
+            a=3*a+1
+        print (a)
+    nbrlter+=1
+    return (nbrlter)
+var = Collatz (int(input()))
+print (var)
